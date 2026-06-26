d0b1443bd1236075e292d1fb5584d9d8340f0f1b
tk Audio Services <tk@tkaudioservices.com>
2026-06-26 11:40:52 +0200

    Ported Sound Panner to Klanglogik

--- SurroundPanner/Launch klSurroundPanner.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem tkSurroundPanner launcher (Windows). Starts the bridge and opens the UI.
-rem First time? Run "Install tkSurroundPanner.bat" to add the panner to REAPER.
-cd /d "%~dp0"
-start "" "http://localhost:9000/"
-python bridge\reaper_bridge.py
-pause
